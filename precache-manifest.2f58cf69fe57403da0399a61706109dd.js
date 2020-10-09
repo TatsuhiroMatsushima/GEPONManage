dc800e10626a7c11939df5b8bfa691730be41628
Tatsuhiro_mac <mtatsu0323@gmail.com>
2020-10-09 16:27:05 +0900

    Updates

---
+++ precache-manifest.2f58cf69fe57403da0399a61706109dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8fff78734132ab07ffc0c3e3ecb5d3",
+    "url": "/GEPONManage/index.html"
+  },
+  {
+    "revision": "6c3461aed3decf5fe054",
+    "url": "/GEPONManage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2cdd39fb30a8b687e85",
+    "url": "/GEPONManage/static/js/2.53a08312.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GEPONManage/static/js/2.53a08312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3461aed3decf5fe054",
+    "url": "/GEPONManage/static/js/main.a4038fa4.chunk.js"
+  },
+  {
+    "revision": "a56e0348f0b03af7ced9",
+    "url": "/GEPONManage/static/js/runtime-main.a3def3a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b08c6897fc297516a31bfe4b1f00d71.js"
+  "/GEPONManage/precache-manifest.2f58cf69fe57403da0399a61706109dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GEPONManage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
